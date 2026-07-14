c63654a01724adf24cbf7d9dd6345d6f90c9d49a
Pasinduhansana <gallagepasinduhansana@gmail.com>
2026-07-14 14:05:36 +0530

    Add Nex Config file for Image Configuration Add Activity card to Page Section

---
+++ src/app/activities/page.js
@@ -0,0 +1,10 @@
+import Activities from "@/components/activities/Activities";
+
+export const metadata = {
+  title: "Activities | Bashitha Ceramics",
+  description: "Track and manage your business activities",
+};
+
+export default function Page() {
+  return <Activities />;
+}
